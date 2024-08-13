1ad267f9a08a21b533066c8bc5e55abfd1f02f0c
MohitProg <mohitjj999@gmail.com>
2024-08-13 16:43:51 +0530

    fi the issue

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://webtalkchat-76b86.web.app",
+   
     credentials:true
 }))
 
